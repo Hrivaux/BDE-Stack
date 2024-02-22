6859220d6af143a568d74fbe84ae5a69693be516
Rayan69100 <yayan1@live.fr>
2024-02-22 12:28:07 +0100

    Mises a jour de l'automatisation de mail pour les evenement a zero participant

--- script/notifierCreateurEvenement.php
+++ script/notifierCreateurEvenement.php
@@ -3,34 +3,30 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-
 require_once('../inc/DataBaseConnection.php');
 require '../vendor/autoload.php'; 
 
-// Connexion à la base de données
-$database = new DatabaseConnection('mysql-hubin.alwaysdata.net', 'hubin_bde', 'hubin', 'HubinSQL2022!');
-$bdd = $database->connect();
 
-// Récupérer tous les événements qui se déroulent dans 5 jours
-$query = $bdd->prepare("SELECT * FROM evenements WHERE DATE(date) = DATE_ADD(CURDATE(), INTERVAL 5 DAY)");
-$query->execute();
-$events = $query->fetchAll();
+// Récupérer tous les événements à J-5
+$requete = $bdd->prepare("SELECT e.id, e.libelle_evenement, e.id_user, e.date, COUNT(i.id) as nb_inscrits
+FROM evenements e
+LEFT JOIN inscriptions_evenements i ON e.id = i.id_evenement AND i.actif = b'1'
+WHERE e.date <= DATE_ADD(CURDATE(), INTERVAL 5 DAY) AND e.date >= CURDATE()
+GROUP BY e.id
+HAVING nb_inscrits = 0;
+");
+$requete->execute();
+$evenements = $requete->fetchAll();
 
-foreach ($events as $event) {
-    // Vérifier s'il y a des inscrits à cet événement
-    $inscriptionQuery = $bdd->prepare("SELECT COUNT(*) FROM inscriptions_evenements WHERE id_evenement = ? AND actif = b'1'");
-    $inscriptionQuery->execute([$event['id']]);
-    $inscriptionCount = $inscriptionQuery->fetchColumn();
+foreach ($evenements as $evenement) {
+    // Si moins de 5 inscrits, envoyer un mail au créateur de l'événement
+    if ($evenement['nb_inscrits'] < 5) {
+        // Récupérer l'email du créateur de l'événement
+        $requeteUser = $bdd->prepare("SELECT email FROM users WHERE id = ?");
+        $requeteUser->execute([$evenement['id_user']]);
+        $createur = $requeteUser->fetch();
 
-    if ($inscriptionCount == 0) {
-        // Aucun inscrit, envoyer un email au créateur de l'événement
-
-        // Récupérer les informations de l'utilisateur (créateur de l'événement)
-        $userQuery = $bdd->prepare("SELECT email, prenom FROM users WHERE id = ?");
-        $userQuery->execute([$event['id_user']]);
-        $user = $userQuery->fetch();
-
-        // Configuration de PHPMailer pour envoyer l'email
+        // Envoyer l'email
         $mail = new PHPMailer(true);
         try {
             $mail->CharSet = 'UTF-8';
@@ -42,12 +38,12 @@ foreach ($events as $event) {
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
-            $mail->setFrom('majtx69@gmail.com', 'BDE');
-            $mail->addAddress($user['email'], $user['prenom']);
+            $mail->setFrom('majtx69@gmail.com', 'BDE'); // Remplacer par votre e-mail et nom
+            $mail->addAddress($createur['email']); // Ajouter l'email du créateur de l'événement
 
             $mail->isHTML(true);
-            $mail->Subject = "Aucun inscrit à votre événement \"" . $event['libelle_evenement'] . "\"";
-            $mail->Body = "Bonjour " . $user['prenom'] . ",<br><br>Nous vous informons qu'il n'y a actuellement aucun inscrit à votre événement \"" . $event['libelle_evenement'] . "\" prévu pour le " . $event['date'] . ".<br><br>Cordialement,<br>L'équipe de gestion des événements";
+            $mail->Subject = 'Peu d\'inscriptions pour votre événement ' . $evenement['libelle_evenement'];
+            $mail->Body = 'Bonjour, <br><br> Nous avons remarqué que votre événement "' . $evenement['libelle_evenement'] . '" prévu le ' . $evenement['date'] . ' a moins de 5 inscriptions. Vous voudrez peut-être prendre des mesures pour promouvoir davantage votre événement. <br><br> Cordialement, <br> Votre équipe';
 
             $mail->send();
         } catch (Exception $e) {
